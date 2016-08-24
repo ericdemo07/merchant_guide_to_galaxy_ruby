610d3f8a5c5ea03586e0e52aa7eac22571dd3b20
Ayush Shukla <ericdemo07@gmail.com>
2016-08-25 01:00:13 +0530

    added third parsing

--- util/parseutil.rb
+++ util/parseutil.rb
@@ -17,10 +17,17 @@ class ParseUtil < ConvertCurrency
         temp =  input_string_currency_hash[matchdata[1]].to_s.concat(input_string_currency_hash[matchdata[2]].to_s)
         integer_value = roman_to_integer temp
         input_string_multp_currency_hash[matchdata[3]] = matchdata[4].to_i/integer_value
+        p input_string_multp_currency_hash
       end
-      p input_string_multp_currency_hash
+      if /^how much is\s([a-zA-Z0-9]+)\s([a-zA-Z0-9]+)\s([a-zA-Z0-9]+)\s([a-zA-Z0-9]+)\s\?$/ =~ input_string   # for input string type how much is pish tegj glob glob ?
+        matchdata = input_string.match(/how much is ([^>]*) ([^>]*) ([^>]*) ([^>]*) ([^>]*) ?/)
+        temp = input_string_currency_hash[matchdata[1]].to_s.concat(input_string_currency_hash[matchdata[2]].to_s).concat(input_string_currency_hash[matchdata[3]].to_s).concat(input_string_currency_hash[matchdata[4]].to_s)
+        integer_value = roman_to_integer temp
+        p matchdata[5]
+        #pish tegj glob glob is 42
+        output = matchdata[1].concat(' ').concat(matchdata[2]).concat(' ').concat(matchdata[3]).concat(' ').concat(matchdata[4]).concat(' is ').concat(integer_value.to_s)
+        p output
+      end
     end
-    #p $roman_values
-    #puts "Doom"
   end
 end
